20529817db336485a256e6bb963b9909717ee47f
johnyjonathan <michaltaszycki@hotmail.com>
2021-03-21 00:27:23 +0100

    Simple front-end

--- picsfolio/views.py
+++ picsfolio/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
+
+def home(request):
+    return render(request,'picsfolio/home.html')
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request,'picsfolio/loginuser.html',{'form':AuthenticationForm()} )
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request,'picsfolio/loginuser.html',{'form':AuthenticationForm(), 'error':'Username or password did not match'})
+        else:
+            login(request, user)
+            return redirect('loggedin')
+
 
 def signupuser(request):
     if request.method == 'GET':
@@ -22,3 +37,11 @@ def signupuser(request):
 
 def loggedin(request):
     return render(request,'picsfolio/loggedin.html')
+
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect ('home')
+    
