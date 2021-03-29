013cffac992ec92f0d1cdec4b4589bbd343cbb52
KornelWitkowski <80117782+KornelWitkowski@users.noreply.github.com>
2021-03-29 22:10:02 +0200

    Add files via upload

--- picsfolio/views.py
+++ picsfolio/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .forms import UserCreationForm, uploadImgForm
-from django import forms
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from django.utils import timezone
 from .models import UserImage
-from .functions import getMetadataFromJpg, ROLimageId,RORimageId
-from django.contrib import messages
-
+from .functions import getMetadataFromJpg
 
 
 def home(request):
@@ -43,41 +40,9 @@ def signupuser(request):
         else:
             return render(request,'picsfolio/signupuser.html',{'form':UserCreationForm(), 'error':'Passwords did not match'} )
 
-
 def loggedin(request):
-
-
-    if request.method == 'GET':
-
-
-        images = UserImage.objects.filter(user=request.user)
-        return render(request,'picsfolio/loggedin.html', {'images':images})
-
-    else:
-        if request.POST.get("previous"):
-
-            ROLimageId(request)
-            images = UserImage.objects.filter(user=request.user)
-            return render(request,'picsfolio/loggedin.html', {'images':images})
-
-        elif request.POST.get("next"):
-
-            RORimageId(request)
-            images = UserImage.objects.filter(user=request.user)
-            return render(request,'picsfolio/loggedin.html', {'images':images})
-
-        elif request.POST.get("delete"):
-
-            images = UserImage.objects.filter(user=request.user)
-            ids = list(images.values_list('id', flat=True))
-            if len(ids) != 0:
-                UserImage.objects.get(id= ids[0]).delete()
-
-            images = UserImage.objects.filter(user=request.user)
-
-            return render(request,'picsfolio/loggedin.html', {'images':images})
-
-    return redirect ('home')
+    images = UserImage.objects.filter(user=request.user)
+    return render(request,'picsfolio/loggedin.html', {'images':images})
 
 
 
@@ -91,21 +56,10 @@ def uploadimg(request):
         return render(request,'picsfolio/uploadimg.html', {'form': uploadImgForm()})
     else:
         form = uploadImgForm(request.POST, request.FILES)
-
-        if not form.is_valid():
-            if not bool(form.cleaned_data.get("name")):
-                messages.info(request, 'Name field can not be empty.')
-                return redirect ('uploadimg')
-        elif form.cleaned_data.get("image") == 'default.jpg':
-            messages.info(request, 'Image field can not be empty.')
-            return redirect ('uploadimg')
-
         newimg = form.save(commit=False)
         newimg.user = request.user
         newimg.save()
-        messages.info(request, 'Your picture has been uploaded successfully!')
         return redirect ('home')
-
 
 def imgmetadata(request,image_id):
     img= get_object_or_404(UserImage, pk=image_id)
