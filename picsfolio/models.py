9f562577c471413145e1bba153a103dae70a4bee
KornelWitkowski <mrkhornel@gmail.com>
2021-04-22 14:18:29 +0200

    final version

--- picsfolio/models.py
+++ picsfolio/models.py
@@ -1,13 +1,175 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
+
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+from fractions import Fraction
+from natsort import natsorted
+
+class imagesMetadataList:
+    def __init__(self, DateTimeOriginal, Model, ExposureTime,FNumber, ISOSpeedRatings, LensModel,View):
+        self.DateTimeOriginal = DateTimeOriginal
+        self.Model = Model
+        self.ExposureTime = ExposureTime
+        self.FNumber = FNumber
+        self.ISOSpeedRatings = ISOSpeedRatings
+        self.LensModel = LensModel
+        self.View = View
+
+
+class UserCatalog(models.Model):
+    CatalogName = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='1')
+
+    def __str__(self):
+        return self.name
+
+    def createImageList(catalogName, request):
+
+        images = []
+
+        if catalogName == "All":
+            imagesNames = UserImage.objects.filter(user=request.user).values_list('name', flat=True)
+        else:
+            imagesNames = UserCatalog.objects.get(user=request.user, CatalogName=catalogName).userimage_set.all()
+
+
+        for IMGname in imagesNames:
+
+            imageA = UserImage.objects.get(name=IMGname,user=request.user)
+            imageFile = Image.open(imageA.image)
+            exifData = imageFile.getexif()
+            metadata = {}
+
+            for tag_id in exifData:
+                tag = TAGS.get(tag_id, tag_id)
+                data = exifData.get(tag_id)
+                metadata[f"{tag}"] = f"{data}"
+
+            nodata = "None"
+
+            try:
+                DateTimeOriginal = metadata["DateTimeOriginal"]
+            except:
+                DateTimeOriginal = nodata
+            try:
+                Model = metadata["Model"]
+            except:
+                Model = nodata
+            try:
+                ExposureTime = str(Fraction(float(metadata["ExposureTime"])).limit_denominator())
+            except:
+                ExposureTime = nodata
+            try:
+                FNumber = metadata["FNumber"]
+            except:
+                FNumber = nodata
+            try:
+                ISOSpeedRatings = metadata["ISOSpeedRatings"]
+            except:
+                ISOSpeedRatings = nodata
+            try:
+                LensModel = metadata["LensModel"]
+            except:
+                LensModel = nodata
+
+            imgMetadataList=imagesMetadataList(DateTimeOriginal, Model, ExposureTime,FNumber, ISOSpeedRatings, LensModel, True)
+
+            images.append([imageA, imgMetadataList])
+
+            if request.method != 'GET' and not request.POST.get("Select"):
+                imagesToShow = list(request.POST.getlist("showCheckbox"))
+                for image, MDlist in images:
+                    if image.name in imagesToShow:
+                        MDlist.View = True
+                    else:
+                        MDlist.View = False
+
+        return images
+
+    def sortImages(request, imagesList, SortParameter):
+
+
+        lista = []
+
+        for image, MD in imagesList:
+            if SortParameter == 'Name':
+                lista.append([image.name, image.id])
+            if SortParameter == 'DateTimeOriginal':
+                lista.append([MD.DateTimeOriginal, image.id])
+            if SortParameter == 'Model':
+                lista.append([MD.Model, image.id])
+            if SortParameter == 'ExposureTime':
+                lista.append([MD.ExposureTime, image.id])
+            if SortParameter == 'FNumber':
+                lista.append([MD.FNumber, image.id])
+            if SortParameter == 'ISOSpeedRatings':
+                lista.append([MD.ISOSpeedRatings, image.id])
+            if SortParameter == 'LensModel':
+                lista.append([MD.LensModel, image.id])
+
+        ids = []
+
+        for x, y in lista:
+            ids.append(y)
+
+        if SortParameter == 'Name':
+            lista.sort(key=lambda y: y[0].lower())
+        else:
+            lista = natsorted(lista, key=lambda y: y[0])
+
+        ids2 = []
+
+        for x, y in lista:
+            ids2.append(y)
+
+        if ids == ids2:
+            ids.reverse()
+
+        for i in range(len(ids)):
+            image = UserImage.objects.get(user=request.user, id=ids2[i])
+            image.id = ids[i] + 10000
+            image.save()
+            UserImage.objects.get(user=request.user, id=ids2[i]).delete()
+
+        for i in range(len(ids2)):
+            image = UserImage.objects.get(user=request.user, id=ids[i] + 10000)
+            image.id = ids[i]
+            image.save()
+            UserImage.objects.get(user=request.user, id=ids[i] + 10000).delete()
+
+        return
+
 
 class UserImage(models.Model):
     name = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to="piscfolio/images", default='default.jpg')
-    user = models.ForeignKey(User, on_delete=models.CASCADE,default='1')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='1')
+    catalog = models.ForeignKey(UserCatalog, on_delete=models.CASCADE, default='1')
     description = models.TextField(blank=True, max_length='1000')
+
 
     def __str__(self):
         return self.name
+
+    def getMetadataFromJpg(image_path):
+
+        image = Image.open(image_path)
+
+        exifdata = image.getexif()
+        metadata = []
+
+        for tag_id in exifdata:
+            tag = TAGS.get(tag_id, tag_id)
+            data = exifdata.get(tag_id)
+            metadata.append(f"{tag} : {data}")
+
+        exifdata = metadata
+
+        if len(exifdata) == 0:
+            exifdata = "The file does not contain any metadata."
+
+        return exifdata
--- picsfolio/views.py
+++ picsfolio/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .forms import UserCreationForm, uploadImgForm
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import UserCreationForm, uploadImgForm, newCatalogForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
-from django.utils import timezone
-from .models import UserImage
-from .functions import getMetadataFromJpg
+from .models import UserImage, UserCatalog
+from django.contrib import messages
 
 
 def home(request):
-    
-    return render(request,'picsfolio/home.html')
+
+    return render(request, 'picsfolio/home.html')
+
 
 def loginuser(request):
     if request.method == 'GET':
-        return render(request,'picsfolio/loginuser.html',{'form':AuthenticationForm()} )
+        return render(request, 'picsfolio/loginuser.html', {'form': AuthenticationForm()})
     else:
         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
-            return render(request,'picsfolio/loginuser.html',{'form':AuthenticationForm(), 'error':'Username or password did not match'})
+            return render(request,'picsfolio/loginuser.html', {'form': AuthenticationForm(), 'error': 'Username or password did not match'})
         else:
             login(request, user)
             return redirect('loggedin')
@@ -38,30 +38,109 @@ def signupuser(request):
             except IntegrityError:
                 return render(request,'picsfolio/signupuser.html',{'form':UserCreationForm(), 'error':'That username has already taken'})
         else:
-            return render(request,'picsfolio/signupuser.html',{'form':UserCreationForm(), 'error':'Passwords did not match'} )
+            return render(request,'picsfolio/signupuser.html',{'form':UserCreationForm(), 'error': 'Passwords did not match'} )
+
 
 def loggedin(request):
-    images = UserImage.objects.filter(user=request.user)
-    return render(request,'picsfolio/loggedin.html', {'images':images})
 
+    catalogs = UserCatalog.objects.filter(user=request.user).values_list('CatalogName', flat=True)
+    images = UserCatalog.createImageList("All", request)
+
+    if request.method == 'GET':
+        return render(request, 'picsfolio/loggedin.html', {'images': images, 'catalogs': catalogs})
+    else:
+        catalogName = request.POST.get("Catalogs")
+        images = UserCatalog.createImageList(catalogName, request)
+
+        if request.POST.get("delete"):
+
+            ids = []
+            for image, MD in images:
+                ids.append(image.id)
+
+            if len(ids) != 0:
+                UserImage.objects.get(id=ids[0]).delete()
+
+            images = UserCatalog.createImageList(catalogName, request)
+
+        elif request.POST.get("deleteCatalog"):
+
+            if catalogName == "All":
+                messages.info(request, "Catalog ""All"" can not be deleted")
+            else:
+                for image, MDlist in images:
+                        UserImage.objects.get(user=request.user, id=image.id).delete()
+
+                UserCatalog.objects.get(CatalogName=catalogName).delete()
+
+                messages.info(request, "Catalog """ + catalogName + " ""have been deleted")
+                catalogName = "All"
+                images = UserCatalog.createImageList(catalogName, request)
+
+        elif request.POST.get("Metadata-sort"):
+
+            UserCatalog.sortImages(request, images, request.POST.get("Metadata-sort"))
+            images = UserCatalog.createImageList(catalogName, request)
+
+    return render(request, 'picsfolio/loggedin.html', {'images': images, 'catalogs': catalogs,'selected': catalogName})
 
 
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
-        return redirect ('home')
+        return redirect('home')
+
 
 def uploadimg(request):
+
+    catalogs = UserCatalog.objects.filter(user=request.user).values_list('CatalogName', flat=True)
+
     if request.method == 'GET':
-        return render(request,'picsfolio/uploadimg.html', {'form': uploadImgForm()})
+        return render(request,'picsfolio/uploadimg.html', {'form': uploadImgForm(), "catalogs": catalogs})
     else:
         form = uploadImgForm(request.POST, request.FILES)
-        newimg = form.save(commit=False)
-        newimg.user = request.user
-        newimg.save()
-        return redirect ('home')
+        CatalogForm = newCatalogForm(request.POST)
 
-def imgmetadata(request,image_id):
-    img= get_object_or_404(UserImage, pk=image_id)
-    exifdata = getMetadataFromJpg(img.image)
-    return render(request,'picsfolio/imgmetadata.html', {'image':img,'exifdata':exifdata})
+        if request.POST.get("upload"):
+            if not form.is_valid():
+                if not bool(form.cleaned_data.get("name")):
+                    messages.info(request, 'Name field can not be empty.')
+                    return redirect('uploadimg')
+            elif form.cleaned_data.get("image") == 'default.jpg':
+                messages.info(request, 'Image field can not be empty.')
+                return redirect('uploadimg')
+
+            if not bool(request.POST.get("catalog")):
+                messages.info(request, 'Choose catalog.')
+                return redirect('uploadimg')
+
+            chosenCatalogName = request.POST.get("catalog")
+            newimg = form.save(commit=False)
+            newimg.user = request.user
+            newimg.catalog = UserCatalog.objects.get(CatalogName=chosenCatalogName)
+            newimg.save()
+
+            messages.success(request, 'Your picture has been uploaded successfully!')
+
+            return render(request,'picsfolio/uploadimg.html', {'form': uploadImgForm(), "catalogs": catalogs, "chosenCatalogName" : chosenCatalogName})
+
+        elif request.POST.get("AddCatalog"):
+
+            if not bool(request.POST.get("CatalogName")):
+                messages.info(request, 'Catalog name field can not be empty!')
+            elif not request.POST.get("CatalogName") in catalogs:
+                newCatalog = CatalogForm.save(commit=False)
+                newCatalog.user = request.user
+                newCatalog.save()
+            else:
+                messages.info(request, 'Catalog name must be unique!')
+
+        return redirect('uploadimg')
+
+
+def imgmetadata(request, image_id):
+
+    img = get_object_or_404(UserImage, pk=image_id)
+    exifdata = UserImage.getMetadataFromJpg(img.image)
+
+    return render(request, 'picsfolio/imgmetadata.html', {'image': img, 'exifdata': exifdata})
